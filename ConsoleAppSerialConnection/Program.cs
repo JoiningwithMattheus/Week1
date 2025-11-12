cfe72eb9f01534dcf289d4af50bde8c4dc5f7017
Mattheus <tai.vinhdn05@gmail.com>
2025-11-12 14:34:22 +0100

    Initialization

---
+++ ConsoleAppSerialConnection/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace ConsoleAppSerialConnection;
+
+using System;
+using System.IO.Ports;
+
+class Program
+{
+    const int BAUDRATE = 11500;
+    const int MAX = 50;
+    static void Main(string[] args)
+    {
+        var Program = new Program();
+        var portNames = Program.PortNames;
+        Console.WriteLine("Available Serial Outputs: ");
+        foreach(var port in portNames)
+        {
+            Console.WriteLine()
+        }
+        Console.WriteLine("Hello, World!");
+    }
+}
